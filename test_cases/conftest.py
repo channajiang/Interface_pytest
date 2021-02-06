e6d88967b1a0df65cabf67429b2bed10f3e11221
channa <channa_jiang@126.com>
2021-02-06 12:29:11 +0800

    try to fix ImportError while loading conftest on jenkins

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,7 +1,7 @@
 import sys
 import pytest
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../libs')))
 from libs.shop import Shop
 from libs.login import Login
 # from delivery_systemB.configs.config import NAME_PSW
